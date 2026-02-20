4c84d0721b1a7e7e75a945e4cf9d3564156b3111
panzhh <zhenhepan@gmail.com>
2026-02-20 11:17:11 -0500

    using env to hold the db url.

--- backend/app.py
+++ backend/app.py
@@ -27,7 +27,8 @@ CORS(app)
 #     BASE_DIR, "libri.db"
 # )
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://library_admin:root@localhost:5432/libri_db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://library_admin:root@localhost:5432/libri_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=30)
